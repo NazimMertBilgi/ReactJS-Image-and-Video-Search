ffb6728edf43999758222cc8da52b182b2fc4e65
NazimMertBilgi <nmbjobs@outlook.com.tr>
2020-04-18 18:57:10 +0300

    Updates

---
+++ precache-manifest.9f3fd144051b5691c602dd2bc7a7bea7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b40738c67f350ee0172578282647b38",
+    "url": "/ReactJS-Image-and-Video-Search/index.html"
+  },
+  {
+    "revision": "b5fda68177a14eb2f196",
+    "url": "/ReactJS-Image-and-Video-Search/static/css/main.73b7f6f8.chunk.css"
+  },
+  {
+    "revision": "eb64c6e707b53ff05ce0",
+    "url": "/ReactJS-Image-and-Video-Search/static/js/2.a9a50ac5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ReactJS-Image-and-Video-Search/static/js/2.a9a50ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5fda68177a14eb2f196",
+    "url": "/ReactJS-Image-and-Video-Search/static/js/main.bd1693df.chunk.js"
+  },
+  {
+    "revision": "0d2a7dca5cd95ecc0faa",
+    "url": "/ReactJS-Image-and-Video-Search/static/js/runtime-main.59766fdb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactJS-Image-and-Video-Search/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactJS-Image-and-Video-Search/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactJS-Image-and-Video-Search/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactJS-Image-and-Video-Search/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactJS-Image-and-Video-Search/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/ReactJS-Image-and-Video-Search/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/ReactJS-Image-and-Video-Search/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/ReactJS-Image-and-Video-Search/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/ReactJS-Image-and-Video-Search/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/ReactJS-Image-and-Video-Search/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "056185cde81c97792f20612cc61e5424",
+    "url": "/ReactJS-Image-and-Video-Search/static/media/sifirbir.056185cd.mp3"
+  }
+]);
